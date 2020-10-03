48dcbe1016d3ec436aae6038d7962d59d96dc081
99002460 <71583095+99002460@users.noreply.github.com>
2020-10-03 18:55:06 +0530

    Delete account.cpp

--- src/account.cpp
+++
@@ -1,5 +0,0 @@
-#include "account.h"
-AccountBase::AccountBase():m_accNumber(""),m_accName("") { }
-AccountBase::AccountBase(std::string num,std::string name):m_accNumber(num),m_accName(name){ }
-
-
